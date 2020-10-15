9ac3ee7b317133e1747e3ce534e6af366b7a0a1c
Ruben Veiga <57599501+misterveiga@users.noreply.github.com>
2020-10-15 18:22:04 +0200

    Minor fix

--- src/main/java/com/misterveiga/cds/listeners/ReactionListener.java
+++ src/main/java/com/misterveiga/cds/listeners/ReactionListener.java
@@ -210,7 +210,7 @@ public class ReactionListener extends ListenerAdapter {
 			sb.append(message.getAuthor().getAsMention())
 					.append(commandChannel.getJDA().getEmoteById(ID_REACTION_REJECT_BAN_REQUEST).getAsMention())
 					.append(" Your ban request against ")
-					.append(reportedUser == null ? "the user " : reportedUser.getAsMention()).append(" (")
+					.append(reportedUser == null ? reportedUserId : reportedUser.getAsMention()).append(" (")
 					.append(reportedUser == null ? "who has left the server" : reportedUser.getId())
 					.append(") has been rejected by ").append(reactee.getAsMention())
 					.append(" and the user has been unmuted.\n\nYour ban request evidence: ");
