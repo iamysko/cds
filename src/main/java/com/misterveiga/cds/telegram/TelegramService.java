e2e1d3c1c44725e11740cba27813568d1d430bde
Ruben Veiga <57599501+misterveiga@users.noreply.github.com>
2020-12-08 12:58:12 +0100

    Fix dev notes

--- src/main/java/com/misterveiga/cds/telegram/TelegramService.java
+++ src/main/java/com/misterveiga/cds/telegram/TelegramService.java
@@ -27,7 +27,7 @@ public class TelegramService {
 	private static Logger log = LoggerFactory.getLogger(TelegramService.class);
 
 	/** The Constant CDS_START. */
-	public static final String CDS_START = "CDS is now connected to Discord. Please give me a few seconds to settle in!\nUpdates: ";
+	public static final String CDS_START = "CDS is now connected to Discord. Please give me a few seconds to settle in!%0AUpdates: ";
 
 	/** The Constant CDS_END. */
 	public static final String CDS_END = "CDS has disconnected from Discord. If you don't hear from me shortly, please call my dad for help.";
