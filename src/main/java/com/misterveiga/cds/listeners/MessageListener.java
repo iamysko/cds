bc21a7f8d4f67010526b27ee053df2fceb3be99e
Ruben Veiga <57599501+misterveiga@users.noreply.github.com>
2020-12-13 16:54:59 +0100

    Modify MessageListener

--- src/main/java/com/misterveiga/cds/listeners/MessageListener.java
+++ src/main/java/com/misterveiga/cds/listeners/MessageListener.java
@@ -149,83 +149,11 @@ public class MessageListener extends ListenerAdapter {
 				break;
 
 			} else if (messageText.matches(RegexConstants.SHOW_BANNED_USERS)) { // LIST BANNED USERS (-show_bans)
-				final List<BannedUser> bannedUsers = cdsData.getBannedUsers();
-				log.info("MongoDB returned {} currently banned users.", bannedUsers.size());
-				final String[][] tableData = new String[bannedUsers.size()][6];
-
-				final String[] headers = { "Banned User DiscordTag", "Banned User ID", "Moderator DiscordTag",
-						"Moderator ID", "Ban Date", "Ban Reason" };
-				for (int c = 0; c < bannedUsers.size(); c++) {
-					tableData[c][0] = bannedUsers.get(c).getBannedUserDiscordTag();
-					tableData[c][1] = String.valueOf(bannedUsers.get(c).getBannedUserId());
-					tableData[c][2] = bannedUsers.get(c).getModeratorDiscordTag();
-					tableData[c][3] = String.valueOf(bannedUsers.get(c).getModeratorUserId());
-					tableData[c][4] = bannedUsers.get(c).getDate().toString();
-					tableData[c][5] = bannedUsers.get(c).getBannedUserReason();
-				}
-				try {
-					final File generatedImage = new File("banned_users.jpg");
-					ImageIO.write(TableUtils.createImageFromData(tableData, headers), "jpg", generatedImage);
-					message.getChannel().sendFile(generatedImage, "banned_users.jpg").queue();
-				} catch (final IOException e) {
-					log.error("Banned users table image could not be generated. Sending plaintext version.");
-					final StringBuilder sb = new StringBuilder();
-					sb.append("**Currently Banned Users**\n");
-					sb.append(
-							"```Banned User DiscordTag | Banned User ID | Moderator DiscordTag | Moderator ID | Ban Date | Ban Reason```\n");
-					sb.append("```");
-					bannedUsers.forEach(bannedUser -> {
-						sb.append(bannedUser.getBannedUserDiscordTag()).append(" | ")
-								.append(bannedUser.getBannedUserId()).append(" | ")
-								.append(bannedUser.getModeratorDiscordTag()).append(" | ")
-								.append(bannedUser.getModeratorUserId()).append(" | ")
-								.append(bannedUser.getDate().toString()).append(" | ")
-								.append(bannedUser.getBannedUserReason());
-					});
-					sb.append("```");
-					message.getChannel().sendMessage(sb.toString()).queue();
-				}
-
+				executeListBannedUsers(message);
 				break;
 
 			} else if (messageText.matches(RegexConstants.SHOW_MUTED_USERS)) { // LIST MUTED USERS (-show_mutes)
-				final List<MutedUser> mutedUsers = cdsData.getMutedUsers();
-				log.info("MongoDB returned {} currently muted users.", mutedUsers.size());
-				final String[][] tableData = new String[mutedUsers.size()][7];
-
-				final String[] headers = { "Muted User DiscordTag", "Muted User ID", "Moderator DiscordTag",
-						"Moderator ID", "Mute Start Date", "Mute End Date", "Mute Reason" };
-				for (int c = 0; c < mutedUsers.size(); c++) {
-					tableData[c][0] = mutedUsers.get(c).getMutedUserDiscordTag();
-					tableData[c][1] = String.valueOf(mutedUsers.get(c).getMutedUserId());
-					tableData[c][2] = mutedUsers.get(c).getModeratorDiscordTag();
-					tableData[c][3] = String.valueOf(mutedUsers.get(c).getModeratorUserId());
-					tableData[c][4] = mutedUsers.get(c).getStartDate().toString();
-					tableData[c][5] = mutedUsers.get(c).getEndDate().toString();
-					tableData[c][6] = mutedUsers.get(c).getMuteReason();
-				}
-				try {
-					final File generatedImage = new File("muted_users.jpg");
-					ImageIO.write(TableUtils.createImageFromData(tableData, headers), "jpg", generatedImage);
-					message.getChannel().sendFile(generatedImage, "muted_users.jpg").queue();
-				} catch (final IOException e) {
-					log.error("Muted users table image could not be generated. Sending plaintext version.");
-					final StringBuilder sb = new StringBuilder();
-					sb.append("**Currently Banned Users**\n");
-					sb.append(
-							"```Banned User DiscordTag | Banned User ID | Moderator DiscordTag | Moderator ID | Ban Date | Ban Reason```\n");
-					sb.append("```");
-					mutedUsers.forEach(mutedUser -> {
-						sb.append(mutedUser.getMutedUserDiscordTag()).append(" | ").append(mutedUser.getMutedUserId())
-								.append(" | ").append(mutedUser.getModeratorDiscordTag()).append(" | ")
-								.append(mutedUser.getModeratorUserId()).append(" | ")
-								.append(mutedUser.getStartDate().toString()).append(" | ")
-								.append(mutedUser.getEndDate().toString()).append(" | ")
-								.append(mutedUser.getMuteReason());
-					});
-					sb.append("```");
-					message.getChannel().sendMessage(sb.toString()).queue();
-				}
+				executeListMutedUsers(message);
 				break;
 			}
 
@@ -293,6 +221,112 @@ public class MessageListener extends ListenerAdapter {
 
 		case -1: // Member with no command roles.
 			break;
+		}
+	}
+
+	private void executeListMutedUsers(final Message message) {
+		final List<MutedUser> mutedUsers = cdsData.getMutedUsers();
+		log.info("MongoDB returned {} currently muted users.", mutedUsers.size());
+		final String[][] tableData = new String[mutedUsers.size()][7];
+
+		final String[] headers = { "Muted User DiscordTag", "Muted User ID", "Moderator DiscordTag", "Moderator ID",
+				"Mute Start Date", "Mute End Date", "Mute Reason" };
+
+		for (int c = 0; c < mutedUsers.size(); c++) {
+			tableData[c][0] = mutedUsers.get(c).getMutedUserDiscordTag();
+			tableData[c][1] = String.valueOf(mutedUsers.get(c).getMutedUserId());
+			tableData[c][2] = mutedUsers.get(c).getModeratorDiscordTag();
+			tableData[c][3] = String.valueOf(mutedUsers.get(c).getModeratorUserId());
+			tableData[c][4] = mutedUsers.get(c).getStartDate().toString();
+			tableData[c][5] = mutedUsers.get(c).getEndDate().toString();
+			tableData[c][6] = mutedUsers.get(c).getMuteReason();
+		}
+		try {
+			final File generatedImage = new File("muted_users.jpg");
+			ImageIO.write(TableUtils.createImageFromData(tableData, headers), "jpg", generatedImage);
+
+			final StringBuilder sb = new StringBuilder();
+			sb.append("**Currently Banned Users**\n");
+			sb.append(
+					"```Banned User DiscordTag | Banned User ID | Moderator DiscordTag | Moderator ID | Ban Date | Ban Reason```\n");
+			sb.append("```");
+			mutedUsers.forEach(mutedUser -> {
+				sb.append(mutedUser.getMutedUserDiscordTag()).append(" | ").append(mutedUser.getMutedUserId())
+						.append(" | ").append(mutedUser.getModeratorDiscordTag()).append(" | ")
+						.append(mutedUser.getModeratorUserId()).append(" | ")
+						.append(mutedUser.getStartDate().toString()).append(" | ")
+						.append(mutedUser.getEndDate().toString()).append(" | ").append(mutedUser.getMuteReason());
+			});
+			sb.append("```");
+			message.getChannel().sendMessage(sb.toString()).addFile(generatedImage, "muted_users.jpg").queue();
+
+		} catch (final IOException e) {
+			log.error("Muted users table image could not be generated. Sending plaintext version.");
+			final StringBuilder sb = new StringBuilder();
+			sb.append("**Currently Banned Users**\n");
+			sb.append(
+					"```Banned User DiscordTag | Banned User ID | Moderator DiscordTag | Moderator ID | Ban Date | Ban Reason```\n");
+			sb.append("```");
+			mutedUsers.forEach(mutedUser -> {
+				sb.append(mutedUser.getMutedUserDiscordTag()).append(" | ").append(mutedUser.getMutedUserId())
+						.append(" | ").append(mutedUser.getModeratorDiscordTag()).append(" | ")
+						.append(mutedUser.getModeratorUserId()).append(" | ")
+						.append(mutedUser.getStartDate().toString()).append(" | ")
+						.append(mutedUser.getEndDate().toString()).append(" | ").append(mutedUser.getMuteReason());
+			});
+			sb.append("```");
+			message.getChannel().sendMessage(sb.toString()).queue();
+		}
+	}
+
+	private void executeListBannedUsers(final Message message) {
+		final List<BannedUser> bannedUsers = cdsData.getBannedUsers();
+		log.info("MongoDB returned {} currently banned users.", bannedUsers.size());
+		final String[][] tableData = new String[bannedUsers.size()][6];
+
+		final String[] headers = { "Banned User DiscordTag", "Banned User ID", "Moderator DiscordTag", "Moderator ID",
+				"Ban Date", "Ban Reason" };
+		for (int c = 0; c < bannedUsers.size(); c++) {
+			tableData[c][0] = bannedUsers.get(c).getBannedUserDiscordTag();
+			tableData[c][1] = String.valueOf(bannedUsers.get(c).getBannedUserId());
+			tableData[c][2] = bannedUsers.get(c).getModeratorDiscordTag();
+			tableData[c][3] = String.valueOf(bannedUsers.get(c).getModeratorUserId());
+			tableData[c][4] = bannedUsers.get(c).getDate().toString();
+			tableData[c][5] = bannedUsers.get(c).getBannedUserReason();
+		}
+		try {
+			final File generatedImage = new File("banned_users.jpg");
+			ImageIO.write(TableUtils.createImageFromData(tableData, headers), "jpg", generatedImage);
+
+			final StringBuilder sb = new StringBuilder();
+			sb.append("**Currently Banned Users**\n");
+			sb.append(
+					"```Banned User DiscordTag | Banned User ID | Moderator DiscordTag | Moderator ID | Ban Date | Ban Reason```\n");
+			sb.append("```");
+			bannedUsers.forEach(bannedUser -> {
+				sb.append(bannedUser.getBannedUserDiscordTag()).append(" | ").append(bannedUser.getBannedUserId())
+						.append(" | ").append(bannedUser.getModeratorDiscordTag()).append(" | ")
+						.append(bannedUser.getModeratorUserId()).append(" | ").append(bannedUser.getDate().toString())
+						.append(" | ").append(bannedUser.getBannedUserReason());
+			});
+			sb.append("```");
+			message.getChannel().sendMessage(sb.toString()).addFile(generatedImage, "banned_users.jpg").queue();
+
+		} catch (final IOException e) {
+			log.error("Banned users table image could not be generated. Sending plaintext version.");
+			final StringBuilder sb = new StringBuilder();
+			sb.append("**Currently Banned Users**\n");
+			sb.append(
+					"```Banned User DiscordTag | Banned User ID | Moderator DiscordTag | Moderator ID | Ban Date | Ban Reason```\n");
+			sb.append("```");
+			bannedUsers.forEach(bannedUser -> {
+				sb.append(bannedUser.getBannedUserDiscordTag()).append(" | ").append(bannedUser.getBannedUserId())
+						.append(" | ").append(bannedUser.getModeratorDiscordTag()).append(" | ")
+						.append(bannedUser.getModeratorUserId()).append(" | ").append(bannedUser.getDate().toString())
+						.append(" | ").append(bannedUser.getBannedUserReason());
+			});
+			sb.append("```");
+			message.getChannel().sendMessage(sb.toString()).queue();
 		}
 	}
 
