c405eaa12f9ed354fe2b25f66dc328adb9b7cfbf
Ruben Veiga <57599501+misterveiga@users.noreply.github.com>
2020-12-13 12:38:06 +0100

    Minor fix

--- src/main/java/com/misterveiga/cds/listeners/ReactionListener.java
+++ src/main/java/com/misterveiga/cds/listeners/ReactionListener.java
@@ -271,6 +271,10 @@ public class ReactionListener extends ListenerAdapter {
 			final Member messageAuthor, final Instant now) {
 		if (alertChannel != null && ChronoUnit.SECONDS.between(lastAlertTime, now) > Properties.ALERT_MODS_COOLDOWN) {
 			lastAlertTime = now;
+			String messageContent = message.getContentStripped();
+			if (messageContent.length() > 200) {
+				messageContent = messageContent.substring(0, 201) + "...";
+			}
 			alertChannel
 					.sendMessage(new StringBuilder()
 							.append(alertChannel.getJDA().getEmoteById(ID_REACTION_ALERT_MODS).getAsMention())
@@ -280,7 +284,7 @@ public class ReactionListener extends ListenerAdapter {
 									.getAsMention())
 							.append(" Alert received from ").append(reactee.getAsMention()).append(" (ID: ")
 							.append(reactee.getId()).append("):\n").append(message.getJumpUrl())
-							.append("\nPreview:\n> ").append(message.getContentStripped())
+							.append("\nPreview:\n> ").append(messageContent)
 							.append("\n*(Access the jump URL to take action. Once finished, react to this message with :z_approve:)*"))
 					.queue(msg -> {
 						msg.delete().queueAfter(2, TimeUnit.HOURS);
