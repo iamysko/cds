c5e3ef6d0554e2aa020cf1257dd6553d2c79a79c
Ruben Veiga <57599501+misterveiga@users.noreply.github.com>
2021-01-09 00:43:25 +0100

    Minor fix

--- src/main/java/com/misterveiga/cds/listeners/ReactionListener.java
+++ src/main/java/com/misterveiga/cds/listeners/ReactionListener.java
@@ -297,8 +297,8 @@ public class ReactionListener extends ListenerAdapter {
 					.append(" (offender ID: ").append(messageAuthor.getId()).append("):\n").append(message.getJumpUrl())
 					.append("\nPreview:\n> ").append(messageContent)
 					.append("\n*(Access the jump URL to take action. Once finished, react to this message with* ")
-					.append("\n⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯")
 					.append(alertChannel.getJDA().getEmoteById(ID_REACTION_APPROVE).getAsMention()).append("*)*"))
+					.append("\n⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯")
 					.allowedMentions(mentionTypes).queue(msg -> {
 						msg.delete().queueAfter(2, TimeUnit.HOURS);
 					}, failure -> {
