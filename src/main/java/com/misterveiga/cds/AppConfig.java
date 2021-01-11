a1ae1c6cdc33d48c115a34a177111b5ae62eaf13
Ruben Veiga <57599501+misterveiga@users.noreply.github.com>
2021-01-11 21:06:14 +0100

    Add remote MongoDB cluster

--- src/main/java/com/misterveiga/cds/AppConfig.java
+++ src/main/java/com/misterveiga/cds/AppConfig.java
@@ -97,8 +97,10 @@ public class AppConfig {
 	 * @throws UnknownHostException the unknown host exception
 	 */
 	@Bean
-	MongoTemplate mongoTemplate() throws UnknownHostException {
-		return new MongoTemplate(MongoClients.create("mongodb://localhost:27017"), "rdss");
+	MongoTemplate mongoTemplate(@Value("${mongo.pwd}") final String pwd) throws UnknownHostException {
+		return new MongoTemplate(MongoClients.create(
+				"mongodb+srv://admin:" + pwd + "@robloxdiscord.w7g7e.mongodb.net/rdss?retryWrites=true&w=majority"),
+				"rdss");
 	}
 
 	@PreDestroy
