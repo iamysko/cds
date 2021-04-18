7cb3cf0f8baeec2755d42db8dde8f663cd7aa720
Liscuate <thegamerfreaker@Gmail.com>
2021-04-18 15:30:29 +0200

    Update ReactionListener.java

--- src/main/java/com/misterveiga/cds/listeners/ReactionListener.java
+++ src/main/java/com/misterveiga/cds/listeners/ReactionListener.java
@@ -156,8 +156,8 @@ public class ReactionListener extends ListenerAdapter {
 
 						case ID_REACTION_QM_30:
 							
-							System.out.print("isStaffOnStaff: " + isStaffOnStaff(reactee, messageAuthor, commandChannel));
-							System.out.print("isInStaffChannel: " + isInStaffChannel(reactee, commandChannel, event.getChannel()));
+							System.out.print("isStaffOnStaff: " + isStaffOnStaff(reactee, messageAuthor, commandChannel) + ", ");
+							System.out.print("isInStaffChannel: " + isInStaffChannel(reactee, commandChannel, event.getChannel()) + ", ");
 							System.out.print("isAnyRole: " + RoleUtils.isAnyRole(reactee, RoleUtils.ROLE_SERVER_MANAGER, RoleUtils.ROLE_COMMUNITY_SUPERVISOR, RoleUtils.ROLE_BOT));
 
 							if (!isStaffOnStaff(reactee, messageAuthor, commandChannel)
