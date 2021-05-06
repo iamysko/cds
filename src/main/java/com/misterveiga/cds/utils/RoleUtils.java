aaa90ff293cbf6c5b46d9bc352e77d1e7fddce79
Ruben Veiga <57599501+misterveiga@users.noreply.github.com>
2021-05-06 22:55:33 +0200

    Update RoleUtils
    Server role names have been changed. TODO: Refactor constant variables to reflect change. Consider using Role snowflake IDs instead.

--- src/main/java/com/misterveiga/cds/utils/RoleUtils.java
+++ src/main/java/com/misterveiga/cds/utils/RoleUtils.java
@@ -15,16 +15,16 @@ import net.dv8tion.jda.api.entities.Role;
 public class RoleUtils {
 
 	/** The Constant ROLE_TRIAL_SUPERVISOR. */
-	public static final String ROLE_TRIAL_SUPERVISOR = "Trial Community Supervisor";
+	public static final String ROLE_TRIAL_SUPERVISOR = "Trial Supervisor";
 
 	/** The Constant ROLE_COMMUNITY_SUPERVISOR. */
-	public static final String ROLE_COMMUNITY_SUPERVISOR = "Community Supervisor";
+	public static final String ROLE_COMMUNITY_SUPERVISOR = "Moderator";
 
 	/** The Constant ROLE_SENIOR_COMMUNITY_SUPERVISOR. */
-	public static final String ROLE_SENIOR_COMMUNITY_SUPERVISOR = "Senior Community Supervisor";
+	public static final String ROLE_SENIOR_COMMUNITY_SUPERVISOR = "Senior Moderator";
 
 	/** The Constant ROLE_SERVER_MANAGER. */
-	public static final String ROLE_SERVER_MANAGER = "Server Manager";
+	public static final String ROLE_SERVER_MANAGER = "Manager";
 
 	/** The Constant ROLE_LEAD. */
 	public static final String ROLE_LEAD = "Lead";
