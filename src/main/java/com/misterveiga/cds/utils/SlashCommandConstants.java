33ea9168dae790fc555bc1894113f80b8cfdcba2
Noah Kok <s1147965@student.windesheim.nl>
2021-11-12 22:16:09 +0100

    Slash Commands update!

---
+++ src/main/java/com/misterveiga/cds/utils/SlashCommandConstants.java
@@ -0,0 +1,9 @@
+package com.misterveiga.cds.utils;
+
+public class SlashCommandConstants {
+
+	public static final String COMMAND_HELP = "help";
+	
+	public static final String COMMAND_ABOUT = "about";
+
+}
