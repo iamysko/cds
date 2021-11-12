dfb2210ab637c162bf2a2bb2a2cb5694878e8fe4
TheMadKingNoah <75645597+TheMadKingNoah@users.noreply.github.com>
2021-11-12 20:53:09 +0100

    Update AlertMaintainerThread.java

--- src/main/java/com/misterveiga/cds/utils/AlertMaintainerThread.java
+++ src/main/java/com/misterveiga/cds/utils/AlertMaintainerThread.java
@@ -36,13 +36,21 @@ public class AlertMaintainerThread {
 		log.debug("[AlertMaintainerThread] Checking for old alerts...");
 
 		final Guild guild = jda.getGuildById(Properties.GUILD_ROBLOX_DISCORD_ID);
+		
+		guild.getTextChannelById(Properties.CHANNEL_MODERATORS_ID).sendMessage(new StringBuilder()
+						.append(RoleUtils.getRoleByName(guild, RoleUtils.ROLE_MODERATOR_NAME).getAsMention())
+						.append("\n**!! Mod Alerts Pending !!**")
+						.append("\nThere are pending mod alerts in #mod-alerts!")
+						.append("\nPlease remember to monitor Mod alerts frequently in order to avoid an accumulation of messages (and untreated reports) in the channel.")
+						.append("\n\n*This message will only appear if there are alerts more than 2 hours old.*"))
+						.queue();
 
 		guild.getTextChannelById(Properties.CHANNEL_MOD_ALERTS_ID).getHistoryFromBeginning(1).queue(messageHistory -> {
 			final Message firstMessage = messageHistory.getRetrievedHistory().get(0);
 			final OffsetDateTime firstMessageDateTime = firstMessage.getTimeCreated();
 			final ZoneOffset firstMessageZone = firstMessageDateTime.getOffset();
 
-			if (!firstMessageDateTime.isBefore(OffsetDateTime.now(firstMessageZone).minusHours(2L))) {
+			if (firstMessageDateTime.isBefore(OffsetDateTime.now(firstMessageZone).minusHours(2L))) {
 				log.info("[AlertMaintainerThread] Alerts over 2 hours old found. Notifying the team...");
 				guild.getTextChannelById(Properties.CHANNEL_MODERATORS_ID).sendMessage(new StringBuilder()
 						.append(RoleUtils.getRoleByName(guild, RoleUtils.ROLE_MODERATOR_NAME).getAsMention())
