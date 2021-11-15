fc9e35f7cf01bbbbae91421d94e3d3bc51700658
Noah <s1147965@student.windesheim.nl>
2021-11-15 14:48:50 +0100

    removed unnecessary includement.

--- src/main/java/com/misterveiga/cds/utils/AlertMaintainerThread.java
+++ src/main/java/com/misterveiga/cds/utils/AlertMaintainerThread.java
@@ -6,7 +6,6 @@ import java.time.ZoneOffset;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.log.LogDelegateFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
