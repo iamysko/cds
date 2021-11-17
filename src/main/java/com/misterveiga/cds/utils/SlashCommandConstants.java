e1cd7841ca24ec22fc93a66fb7ef4ed3fedf1e87
Noah <s1147965@student.windesheim.nl>
2021-11-17 03:00:47 +0100

    bug fixes

--- src/main/java/com/misterveiga/cds/utils/SlashCommandConstants.java
+++ src/main/java/com/misterveiga/cds/utils/SlashCommandConstants.java
@@ -7,5 +7,7 @@ public class SlashCommandConstants {
 	public static final String COMMAND_ABOUT = "about";
 	
 	public static final String COMMAND_USER_INFO = "user-info";
+	
+	public static final String COMMAND_ROLE_USER_LIST = "get-list-users";
 
 }
