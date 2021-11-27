4ad9197b120a795a614e2a25fc97c56ec3f9f1df
James <59822256+Archasion@users.noreply.github.com>
2021-11-27 17:49:46 +0000

    Update EmbedBuilds.java

--- src/main/java/com/misterveiga/cds/utils/EmbedBuilds.java
+++ src/main/java/com/misterveiga/cds/utils/EmbedBuilds.java
@@ -150,18 +150,18 @@ public class EmbedBuilds {
 	}
 	
 	public static EmbedBuilder alertMaintainerEmbed() {
-		EmbedBuilder embed = new EmbedBuilder();
+		EmbedBuilder embed = new EmbedBuilder()
 		
-		embed.setColor(0x748bd8);
-		embed.setTitle("Moderation Alerts Pending");
-		embed.setDescription(new StringBuilder()
+		.setColor(0x748bd8)
+		.setTitle("Moderation Alerts Pending")
+		.setDescription(new StringBuilder()
 				     
 				    .append("There are pending mod alerts in ")
 				    .append(String.format("<#%d>", Properties.CHANNEL_MOD_ALERTS_ID))
 				    .append("\n\n")
-				    .append("Please remember to monitor moderation alerts frequently in order to avoid an accumulation of messages (and untreated reports) in the channel."));
+				    .append("Please remember to monitor moderation alerts frequently in order to avoid an accumulation of messages (and untreated reports) in the channel."))
 		
-		embed.setFooter("This message appears whenever there are alerts that are over 2 hours old.");
+		.setFooter("This message appears whenever there are alerts that are over 2 hours old.");
 		
 		return embed;
 	}
