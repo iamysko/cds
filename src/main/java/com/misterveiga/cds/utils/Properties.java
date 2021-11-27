522069c9328d502047bd2c780d7aa805fb6ec7d8
James <59822256+Archasion@users.noreply.github.com>
2021-11-27 20:11:04 +0000

    Corrected comment

--- src/main/java/com/misterveiga/cds/utils/Properties.java
+++ src/main/java/com/misterveiga/cds/utils/Properties.java
@@ -23,7 +23,7 @@ public class Properties {
 	/** The Constant CHANNEL_COMMANDS_ID. */
 	public static final Long CHANNEL_COMMANDS_ID = 150250250471342080L;
 	
-	/** The Constant CHANNEL_MODERATORS_ID. */
+	/** The Constant CHANNEL_TRIAL_MODERATORS_ID. */
 	public static final Long CHANNEL_TRIAL_MODERATORS_ID = 678671353473269799L;
 
 	/** The Constant CHANNEL_MODERATORS_ID. */
