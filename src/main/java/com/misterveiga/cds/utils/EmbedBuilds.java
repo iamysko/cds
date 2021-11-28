b1bca443f9b33f6c0cde41132b5eceedfc455777
Ruben Veiga <57599501+misterveiga@users.noreply.github.com>
2021-11-28 14:42:29 +0100

    Revert "Replacing the alert maintainer message with an embed & Sending the alert in the trial mod channel"

--- src/main/java/com/misterveiga/cds/utils/EmbedBuilds.java
+++ src/main/java/com/misterveiga/cds/utils/EmbedBuilds.java
@@ -148,23 +148,6 @@ public class EmbedBuilds {
 			return embed;
 		}
 	}
-	
-	public static EmbedBuilder alertMaintainerEmbed() {
-		EmbedBuilder embed = new EmbedBuilder()
-		
-		.setColor(0x748bd8)
-		.setTitle("Moderation Alerts")
-		.setDescription(new StringBuilder()
-				     
-				    .append("There are pending moderation alerts in ")
-				    .append(String.format("<#%d>", Properties.CHANNEL_MOD_ALERTS_ID))
-				    .append("\n\n")
-				    .append("Please remember to monitor moderation alerts frequently in order to avoid an accumulation of messages (and untreated reports) in the channel."))
-		
-		.setFooter("This message appears whenever there are alerts that are over 2 hours old.");
-		
-		return embed;
-	}
 
 	public static EmbedBuilder scanUrl(String url, String botIcon) throws InterruptedException {
 		try {
