86661d3403c085ae236feb381b6ba1b60252ecb4
James <59822256+Archasion@users.noreply.github.com>
2021-11-28 14:27:03 +0000

    Update AlertMaintainerThread.java

--- src/main/java/com/misterveiga/cds/utils/AlertMaintainerThread.java
+++ src/main/java/com/misterveiga/cds/utils/AlertMaintainerThread.java
@@ -48,7 +48,7 @@ public class AlertMaintainerThread {
 				EmbedBuilder embed = EmbedBuilds.alertMaintainerEmbed();
 				
 				log.info("[AlertMaintainerThread] Alerts over 2 hours old found. Notifying the team...");
-				guild.getTextChannelById(Properties.CHANNEL_TRIAL_MODERATORS_ID).sendMessage("@here Pending moderation alerts").queue(messageWithoutEmbed => messageWithoutEmbed.editMessageEmbeds(embed).queue());
+				guild.getTextChannelById(Properties.CHANNEL_TRIAL_MODERATORS_ID).sendMessage("@here Pending moderation alerts").setEmbeds(embed.build()).queue();
 			}
 			}
 		});
