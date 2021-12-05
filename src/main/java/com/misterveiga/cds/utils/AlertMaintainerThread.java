eaf7c645aacf7a883b1b00d42aa1f9160ca12189
James <59822256+Archasion@users.noreply.github.com>
2021-12-05 20:35:00 +0000

    Imported lists and collectors

--- src/main/java/com/misterveiga/cds/utils/AlertMaintainerThread.java
+++ src/main/java/com/misterveiga/cds/utils/AlertMaintainerThread.java
@@ -2,6 +2,8 @@ package com.misterveiga.cds.utils;
 
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
