4b64e52e6587b6fa9fc9d47b604a5e3fde29f763
James <59822256+Archasion@users.noreply.github.com>
2021-12-16 09:11:47 +0000

    Increase the delay between ban request alerts

--- src/main/java/com/misterveiga/cds/utils/AlertMaintainerThread.java
+++ src/main/java/com/misterveiga/cds/utils/AlertMaintainerThread.java
@@ -33,44 +33,49 @@ public class AlertMaintainerThread {
 
 	}
 
-	@Scheduled(fixedDelay = 3600000)
-	public void checkMutes() {
+	@Scheduled(fixedDelay = 86400000)
+	public void BanRequestAlertMaintainer() {
 
-		log.debug("[AlertMaintainerThread] Checking for old ban requests and moderation alerts...");
+		log.debug("[AlertMaintainerThread] Checking for old ban requests...");
 		final Guild guild = jda.getGuildById(Properties.GUILD_ROBLOX_DISCORD_ID);
 
-		// Alert the team if any moderation alerts are 2+ hours old
-		guild.getTextChannelById(Properties.CHANNEL_MOD_ALERTS_ID).getHistoryFromBeginning(1).queue(messageHistory -> {
-			if(!messageHistory.isEmpty()) {
-			final Message firstMessage = messageHistory.getRetrievedHistory().get(0);
-			final OffsetDateTime firstMessageDateTime = firstMessage.getTimeCreated();
-			final ZoneOffset firstMessageZone = firstMessageDateTime.getOffset();
-			
-			if (firstMessageDateTime.isBefore(OffsetDateTime.now(firstMessageZone).minusHours(2L))) {
-        			EmbedBuilder embed = EmbedBuilds.alertMaintainerEmbed();
-
-				log.info("[AlertMaintainerThread] Alerts over 2 hours old found. Notifying the team...");
-				guild.getTextChannelById(Properties.CHANNEL_MODERATORS_ID).sendMessage("@here Pending moderation alerts").setEmbeds(embed.build()).queue();
-			}
-			}
-		});
-		
 		// Alert senior moderators if there are 25+ unreviewed ban requests
 		guild.getTextChannelById(Properties.CHANNEL_BAN_REQUESTS_QUEUE_ID).getIterableHistory().takeAsync(200).thenAccept(messages -> {
-				final List<Message> banRequests = messages.stream()
-						.filter(message -> message.getReactions().isEmpty() && message.getContentRaw().matches("(?i)^;(force)?ban.*$"))
-						.collect(Collectors.toList());
+			final List<Message> banRequests = messages.stream()
+					.filter(message -> message.getReactions().isEmpty() && message.getContentRaw().matches("(?i)^;(force)?ban.*$"))
+					.collect(Collectors.toList());
 
-				if (banRequests.size() >= 25) {
-					StringBuilder sb = new StringBuilder()
+			if (banRequests.size() >= 5) {
+				StringBuilder sb = new StringBuilder()
 						.append(String.format("<@&%d> There are ", RoleUtils.ROLE_SENIOR_MODERATOR))
 						.append(String.format("%d unreviewed ban requests. ", banRequests.size()))
 						.append(String.format("Starting from this message:%n<%s>", banRequests.get(banRequests.size() - 1).getJumpUrl()));
-					
-					log.info("[AlertMaintainerThread] 25+ Ban requests found. Notifying senior moderators...");
-					guild.getTextChannelById(Properties.CHANNEL_SENIOR_MODERATORS_ID).sendMessage(sb.toString()).queue();
-				}
+
+				log.info("[AlertMaintainerThread] 25+ Ban requests found. Notifying senior moderators...");
+				guild.getTextChannelById(Properties.CHANNEL_SENIOR_MODERATORS_ID).sendMessage(sb.toString()).queue();
+			}
 		});
-		
+	}
+
+	@Scheduled(fixedDelay = 3600000)
+	public void ModAlertMaintainer() {
+
+		log.debug("[AlertMaintainerThread] Checking for old alerts...");
+		final Guild guild = jda.getGuildById(Properties.GUILD_ROBLOX_DISCORD_ID);
+
+		guild.getTextChannelById(Properties.CHANNEL_MOD_ALERTS_ID).getHistoryFromBeginning(1).queue(messageHistory -> {
+			if (!messageHistory.isEmpty()) {
+				final Message firstMessage = messageHistory.getRetrievedHistory().get(0);
+				final OffsetDateTime firstMessageDateTime = firstMessage.getTimeCreated();
+				final ZoneOffset firstMessageZone = firstMessageDateTime.getOffset();
+
+				if (firstMessageDateTime.isBefore(OffsetDateTime.now(firstMessageZone).minusSeconds(5L))) {
+					EmbedBuilder embed = EmbedBuilds.alertMaintainerEmbed();
+
+					log.info("[AlertMaintainerThread] Alerts over 2 hours old found. Notifying the team...");
+					guild.getTextChannelById(Properties.CHANNEL_MODERATORS_ID).sendMessage("@here Pending moderation alerts").setEmbeds(embed.build()).queue();
+				}
+			}
+		});
 	}
 }
